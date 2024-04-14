b83073bfe0e2a5e0ae28e4ad442c895f64196f70
padmavathi <padmavathichilamala@gmail.com>
2024-04-15 04:35:02 +0530

    this project containa all Models

---
+++ ModelLayer/Entities/UserCollaborator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Entities
+{
+    public class UserCollaborator
+    {
+        [Key]
+        public string CollaboratorId { get; set; } = "";
+
+        [ForeignKey("UserNote")]
+        public string NoteId { get; set; } = "";
+
+        [ForeignKey("User")]
+        public string CollaboratorEmail { get; set; } = "";
+    }
+}
---
+++ ModelLayer/Entities/UserNote.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Entities
+{
+    public class UserNote
+    {
+        [Key]
+        public string NoteId { get; set; } = "";
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public DateTime reminder { get; set; }
+        public string isArchive { get; set; } = "";
+        public string isPinned { get; set; } = "";
+        public string isTrash { get; set; }
+
+
+        [ForeignKey("User")]
+        public string EmailId { get; set; } = "";
+    }
+}
